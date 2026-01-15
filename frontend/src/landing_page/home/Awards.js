b7f83978018c1f492128b94165c5d11aa9534a68
ShelkePravin <pravinshelke844@gmail.com>
2026-01-15 19:04:29 +0530

    Frontend: added new feature

---
+++ frontend/src/landing_page/home/Awards.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Award() {
+    return ( 
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col-6'>
+                    <img src='media/images/largestBroker.svg' alt='BokerImage' />
+                </div>
+                <div className='col-6'>
+                    <h1>Largest stock in India</h1>
+                </div>
+            </div>
+        </div>
+     );
+}
+
+export default Award;
