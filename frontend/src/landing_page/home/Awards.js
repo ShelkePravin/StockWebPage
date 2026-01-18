fbc079dca641d4cbbd5e8c3684196fbb5d2f25cb
ShelkePravin <pravinshelke844@gmail.com>
2026-01-18 14:19:14 +0530

    finish home 2 components

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -1,18 +1,48 @@
 import React from 'react';
 
 function Award() {
-    return ( 
+    return (
         <div className='container mt-5'>
             <div className='row'>
-                <div className='col-6'>
+                <div className='col-6 p-5'>
                     <img src='media/images/largestBroker.svg' alt='BokerImage' />
                 </div>
-                <div className='col-6'>
-                    <h1>Largest stock in India</h1>
+                <div className='col-6 p-5 mt-5'>
+                    <h1 className=''>Largest stock broker in India</h1>
+                    <p className='mb-5'>2+ million Zerodha clients contribute to over 15% of all retail order volumes in india daily by trading and investing in:</p>
+                    <div className='row'>
+                        <div className='col-6'>
+                            <ul>
+                                <li>
+                                    <p>Futures and Options</p>
+                                </li>
+                                <li>
+                                    <p>Commidity derivatives</p>
+                                </li>
+                                <li>
+                                    <p>Futures and Options</p>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='col-6'>
+                            <ul>
+                                <li>
+                                    <p>Stocks & IPOs</p>
+                                </li>
+                                <li>
+                                    <p>Direct Mutual Funds</p>
+                                </li>
+                                <li>
+                                    <p>Bonds and Govt. Securites</p>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                    <img src='media/images/pressLogos.png' alt='' style={{width: '90%'}}/>
                 </div>
             </div>
         </div>
-     );
+    );
 }
 
 export default Award;
